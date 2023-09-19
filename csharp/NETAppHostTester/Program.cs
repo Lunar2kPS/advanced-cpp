d7f3ad1492081cff0df8fbd1b1a244a9e761dae9
Carlos DaLomba <carlos@2kpixelstudios.net>
2023-09-18 23:16:01 -0400

    Deleted .NET App Host Tester csproj

--- csharp/NETAppHostTester/Program.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace NETAppHostTester {
-    public class Program {
-        public static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}

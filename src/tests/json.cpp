076bc33a11dda1044fa8a894b39c7428b080f2db
Carlos DaLomba <carlos@2kpixelstudios.net>
2023-09-24 02:49:15 -0500

    EXAMPLE WORKS! (nlohmann/json on glm::vec3!)

--- src/tests/json.cpp
+++ src/tests/json.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <string>
 
 #include "nlohmann/json.hpp"
-#include "json.h"
+#include "glm/glm.hpp"
 
 using std::cout;
 using std::endl;
 using std::getline;
 
 using std::ifstream;
-using std::string;
-using std::stringstream;
 
 using json = nlohmann::json;
+using glm::vec3;
+
+namespace nlohmann {
+    template <>
+    struct adl_serializer<vec3> {
+        static void to_json(json& j, const vec3& value) {
+            j = json({
+                {"x", value.x},
+                {"y", value.y},
+                {"z", value.z}
+            });
+        }
+        
+        static void from_json(const json& j, vec3& value) {
+            j.at("x").get_to(value.x);
+            j.at("y").get_to(value.y);
+            j.at("z").get_to(value.z);
+        }
+    };
+}
 
 void testJSON() {
     cout << "JSON testing..." << endl;
-
-    ifstream file = ifstream("resources/Example.json");
-    json test = json::parse(file);
-
-    // string line;
-    // stringstream fileText;
-    // if (file.is_open()) {
-    //     while (file.good()) {
-    //         getline(file, line);
-    //         fileText << line << "\n";
-    //     }
-    // }
-
-    // cout << "File text:\n" << fileText.str() << endl;
     
-    cout << "json = " << test.dump(4) << endl;
-    cout << "json[\"people\"][\"Carlos\"] = " << test["people"]["Carlos"] << endl;
+    vec3 position = vec3(2, -9, 6);
+    json jPosition = position; //ERROR: no suitable user-defined conversion from "glm::vec3" to "json" existsC/C++(312)
+
+    cout << jPosition.dump(4) << endl;
+    cout << endl;
 }

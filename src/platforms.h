dbd5d1e7287d459a42a25bd80e10bb785a7dcbae
Carlos DaLomba <carlos@2kpixelstudios.net>
2023-10-14 17:51:19 -0400

    Added basic platform OS/bitness/architecture functions

--- src/platforms.h
+++ src/platforms.h
@@ -1,16 +1,31 @@
 #pragma once
 
-#if defined _WIN32
+#if defined(_WIN32)
     #define WINDOWS
-#elif defined __APPLE__
+#elif defined(__APPLE__)
     #define MACOS
-#elif defined __linux__
+#elif defined(__linux__)
     #define LINUX
 #else
     #define UNKNOWN_PLATFORM
 #endif
 
-inline const char* getPlatformName() {
+//NOTE: (MSVC flag || Clang/GCC flag)
+#if defined(_M_IX86) || defined(__i386__)
+    #define INTEL
+    #define BIT32
+#elif defined(_M_X64) || defined(__x86_64__)
+    #define INTEL
+    #define BIT64
+#elif defined(_M_ARM) || defined(__arm__)
+    #define ARM
+    #define BIT32
+#elif defined(_M_ARM64) || defined(__aarch64__)
+    #define ARM
+    #define BIT64
+#endif
+
+inline const char* getOSName() {
     return 
 #if defined WINDOWS
         "Windows"
@@ -23,3 +38,28 @@ inline const char* getPlatformName() {
 #endif
     ;
 }
+
+inline const char* getBitness() {
+    return
+#if defined BIT32
+        "32bit"
+#elif defined BIT64
+        "64bit"
+#else
+        "UNKNOWN"
+#endif
+    ;
+}
+
+inline const char* getCPUArchitecture() {
+    return
+#if defined INTEL
+        "Intel"
+#elif defined ARM
+        "Arm"
+#else
+        "UNKNOWN"
+#endif
+    ;
+}
+

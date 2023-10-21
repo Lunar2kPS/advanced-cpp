0b751a5c9b027e6600d865573a11a7de7647b886
Carlos DaLomba <carlos@2kpixelstudios.net>
2023-10-21 01:00:27 -0400

    WIP debugging segmentation fault with VAOs

--- src/opengltester.cpp
+++ src/opengltester.cpp
@@ -1,9 +1,14 @@
 ﻿#include "opengltester.h"
 
+#include <iostream>
+
 #include "glad/gl.h"
 #include "GLFW/glfw3.h"
 
 #include "openglutility.h"
+
+using std::cout;
+using std::endl;
 
 //NOTE: VS Code tooltips with default C/C++ extension --
 //  Using "@brief" for Doxygen comments causes newlines to become ignored,
@@ -21,16 +26,59 @@ static GLuint vao;
 /// @note - VBOs are arrays where you store a (typically huge number of) vertex attributes (positions, normals, uvs, colors, etc.).
 static GLuint vbo;
 
+static GLuint ibo;
+
+static GLuint* indices;
+static int indexCount;
+static float* positions;
+static int positionCount;
+
 void onGLEnable() {
-    GLCALL(glGenVertexArrays(1, &vao));
+    indexCount = 3; //6;
+    positionCount = 8;
+
+    indices = new GLuint[indexCount] {
+        0, 1, 2 //,
+        // 3, 2, 1
+    };
+
+    positions = new float[positionCount] {
+         0.5f, -0.5f,
+        -0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f
+    };
+
     GLCALL(glGenBuffers(1, &vbo));
+    GLCALL(glBindBuffer(GL_ARRAY_BUFFER, vbo));
+    GLCALL(glBufferData(GL_ARRAY_BUFFER, positionCount * sizeof(float), positions, GL_STATIC_DRAW));
+    GLCALL(glBindBuffer(GL_ARRAY_BUFFER, NULL)); //TODO: Test if NULL is defined on platforms other than Windows, cause nullptr doesn't work here
+
+    GLCALL(glGenBuffers(1, &ibo));
+    GLCALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    GLCALL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexCount * sizeof(GLuint), indices, GL_STATIC_DRAW));
+    GLCALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, NULL)); //TODO: Test if NULL is defined on platforms other than Windows, cause nullptr doesn't work here
+
+    // unsigned int offset = 0;
+    // GLCALL(glGenVertexArrays(1, &vao));
+    // GLCALL(glBindVertexArray(vao));
+    // GLCALL(glEnableVertexAttribArray(0));
+    // GLCALL(glVertexAttribPointer(0, 2, GL_FLOAT, false, 2 * sizeof(float), (const void*) offset)); //NOTE: This requires a currently-bound GL_ARRAY_BUFFER target
 }
 
 void onGLDisable() {
-    GLCALL(glDeleteVertexArrays(1, &vao));
+    // GLCALL(glDeleteVertexArrays(1, &vao));
     GLCALL(glDeleteBuffers(1, &vbo));
+    GLCALL(glDeleteBuffers(1, &ibo));
+
+    delete[] indices;
+    delete[] positions;
 }
 
 void onGLUpdate() {
-    // glDrawElements
+    // GLCALL(glBindVertexArray(vao));
+    // GLCALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    // GLCALL(glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, NULL));
+    // GLCALL(glBindVertexArray(NULL));
+    // GLCALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, NULL));
 }
--- src/openglutility.h
+++ src/openglutility.h
@@ -5,12 +5,16 @@
 
 using std::cout;
 using std::endl;
+using std::hex;
+using std::dec;
 
 /// @brief Wraps OpenGL calls with error-logging.
 /// @param innerCall The OpenGL call, such as glClear, glGenVertexArrays, glDrawElements, etc.
+/// @note See: [OpenGL error codes](https://www.khronos.org/opengl/wiki/OpenGL_Error)
 #define GLCALL(innerCall) glClearAllErrors();\
     innerCall;\
-    glLogAllErrors();
+    if (glLogAllErrors() > 0)\
+        cout << "    from:  " << #innerCall << "\n" << endl;
 
 void glClearAllErrors() {
     while (glGetError() != GL_NO_ERROR);
@@ -22,7 +26,7 @@ int glLogAllErrors() {
     GLenum error;
     int count = 0;
     while ((error = glGetError()) != GL_NO_ERROR) {
-        cout << "[OpenGL Error] (" << error << ")" << endl;
+        cout << "[OpenGL Error] (" << error << " => 0x" << hex << error << dec << ")" << endl;
         count++;
     }
     return count;

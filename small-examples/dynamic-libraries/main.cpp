d6c1d5987fa6e530d16f08d8f1521cfda2d83e90
Carlos DaLomba <carlos@2kpixelstudios.net>
2023-12-29 00:54:04 -0600

    Added CMake example with 1 main + 1 dynamic library!
    fixes CORE-37

--- small-examples/dynamic-libraries/main.cpp
+++ small-examples/dynamic-libraries/main.cpp
@@ -1,9 +1,14 @@
 ﻿#include <iostream>
+#include "library/SomeClass.h"
 
 using std::cout;
 using std::endl;
 
+using namespace examplelibrary;
+
 int main() {
     cout << "(Small) main program!" << endl;
+    SomeClass c = SomeClass("Test");
+    cout << "c.getName() = " << c.getName() << endl;
     return 0;
 }

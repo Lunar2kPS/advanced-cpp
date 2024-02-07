5325f775a824cee6c7347e5d5c8ea0b5190b2c51
Carlos DaLomba <carlos@2kpixelstudios.net>
2024-02-06 23:09:21 -0600

    Started testing with simdjson, but ONLY SUPPORTS READING! Ugh!

---
+++ small-examples/json2/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+#include "simdjson/simdjson.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+using namespace simdjson;
+using namespace simdjson::ondemand;
+
+int main() {
+    string someJSON = R"(
+{
+    "value": 49.3,
+    "value2": 0,
+    "name": "hi"
+}
+)";
+
+    padded_string someJSONPadded = padded_string(someJSON);
+
+    parser parser;
+    
+    document doc = parser.iterate(someJSONPadded);
+    size_t count = doc.count_elements();
+
+
+    return 0;
+}

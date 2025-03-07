61f266b89c8d2aa0ff4d75f2053992e7e02b7b0b
Carlos DaLomba <carlos@2kpixelstudios.net>
2025-03-07 08:47:01 -0600

    Demonstrated XAudio2 waiting for audio playback completion with IXAudio2VoiceCallback + EVENT

--- small-examples/win32-xaudio2/src/main.cpp
+++ small-examples/win32-xaudio2/src/main.cpp
@@ -19,30 +19,6 @@ using std::getline;
 namespace this_thread = std::this_thread;
 namespace chrono = std::chrono;
 
-//NOTE: Just here for RAII:
-struct MainProgramData {
-    bool initializedXAudio2 = false;
-    IXAudio2* audio = nullptr;
-    IXAudio2MasteringVoice* master = nullptr;
-    IXAudio2SourceVoice* voice = nullptr;
-    BYTE* audioData;
-    bool isPlaying = false;
-
-    ~MainProgramData() {
-        if (voice != nullptr)
-        voice->DestroyVoice();
-        if (master != nullptr)
-            master->DestroyVoice();
-        if (audio != nullptr)
-            audio->Release();
-
-        if (audioData != nullptr)
-            delete[] audioData;
-        if (initializedXAudio2)
-        CoUninitialize();
-    }
-};
-
 /// @brief The data contained from the beginning of a .wav file.
 struct WavHeader {
     char riff[4];
@@ -58,6 +34,58 @@ struct WavHeader {
     uint16_t bitsPerSample;
     char data[4];
     uint32_t dataSize;
+};
+
+class VoiceCallback : public IXAudio2VoiceCallback {
+    private:
+        HANDLE event;
+
+    public:
+        VoiceCallback() {
+            event = CreateEvent(nullptr, false, false, nullptr);
+        }
+
+        ~VoiceCallback() {
+            CloseHandle(event);
+        }
+
+        HANDLE getEvent() { return event; }
+
+        void OnBufferEnd(void* bufferContext) override {
+            SetEvent(event);
+        }
+
+        void OnVoiceProcessingPassStart(UINT32 bytesRequired) override { }
+        void OnVoiceProcessingPassEnd() override { }
+        void OnStreamEnd() override { }
+        void OnBufferStart(void* bufferContext) override { }
+        void OnLoopEnd(void* bufferContext) override { }
+        void OnVoiceError(void* bufferContext, HRESULT error) override { }
+};
+
+//NOTE: Just here for RAII:
+struct MainProgramData {
+    bool initializedXAudio2 = false;
+    IXAudio2* audio = nullptr;
+    IXAudio2MasteringVoice* master = nullptr;
+    IXAudio2SourceVoice* voice = nullptr;
+    BYTE* audioData;
+    bool isPlaying = false;
+    VoiceCallback voiceCallback = { };
+
+    ~MainProgramData() {
+        if (voice != nullptr)
+        voice->DestroyVoice();
+        if (master != nullptr)
+            master->DestroyVoice();
+        if (audio != nullptr)
+            audio->Release();
+
+        if (audioData != nullptr)
+            delete[] audioData;
+        if (initializedXAudio2)
+        CoUninitialize();
+    }
 };
 
 bool loadWavFile(const char* filePath, WavHeader* result, BYTE** data) {
@@ -123,7 +151,7 @@ int main() {
         format.wBitsPerSample = wavHeader.bitsPerSample;
         format.cbSize = 0;
 
-        result = data.audio->CreateSourceVoice(&data.voice, &format);
+        result = data.audio->CreateSourceVoice(&data.voice, &format, 0, XAUDIO2_DEFAULT_FREQ_RATIO, &data.voiceCallback);
         if (result != S_OK) {
             cerr << "Failed to create audio voice for playback." << endl;
             throw 4;
@@ -145,7 +173,17 @@ int main() {
             cerr << "Failed to play audio file." << endl;
         } else {
             data.isPlaying = true;
-            this_thread::sleep_for(chrono::milliseconds(5000));
+
+            //NOTE: This waits in one go for the full audio to finish:
+            // WaitForSingleObject(data.voiceCallback.getEvent(), INFINITE);
+
+            //NOTE: This loops, demonstrating potentially doing other work, while polling to see if the audio is finished:
+            while (data.isPlaying) {
+                this_thread::sleep_for(chrono::milliseconds(50));
+                cout << "... (50ms)" << endl;
+                data.isPlaying = WaitForSingleObject(data.voiceCallback.getEvent(), 0) == WAIT_TIMEOUT;
+            }
+            cout << "Audio playback complete!" << endl;
         }
 
         return 0;

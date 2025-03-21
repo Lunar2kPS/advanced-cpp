a0270c0a0bba5b5faf8dc9a40a249cf3f95586d0
Carlos DaLomba <carlos@2kpixelstudios.net>
2025-03-20 21:18:40 -0500

    Added example program with C++ 20 coroutines

---
+++ small-examples/game-coroutines-cpp20/src/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <coroutine>
+
+using std::cout;
+using std::endl;
+
+using std::suspend_always;
+using std::suspend_never;
+
+struct CustomTask {
+    struct promise_type {
+        CustomTask get_return_object() { return { }; }
+        suspend_never initial_suspend() { return { }; }
+        suspend_never final_suspend() noexcept { return { }; }
+        void return_void() { }
+        void unhandled_exception() { }
+    };
+};
+
+CustomTask exampleCoroutine() {
+    cout << "Statement A" << endl;
+    co_await suspend_always { };
+    cout << "Statement B" << endl;
+}
+
+int main() {
+    exampleCoroutine();
+    return 0;
+}

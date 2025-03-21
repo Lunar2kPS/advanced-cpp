02cdf14f0899622cd8403ab19cc421a54737e51a
Carlos DaLomba <carlos@2kpixelstudios.net>
2025-03-20 21:57:36 -0500

    Demonstrated coroutine resuming (via coroutine_handles)

--- small-examples/game-coroutines-cpp20/src/main.cpp
+++ small-examples/game-coroutines-cpp20/src/main.cpp
@@ -3,18 +3,25 @@
 
 using std::cout;
 using std::endl;
+using std::boolalpha;
 
 using std::suspend_always;
 using std::suspend_never;
+using std::coroutine_handle;
 
 struct CustomTask {
     struct promise_type {
-        CustomTask get_return_object() { return { }; }
+        CustomTask get_return_object() { return CustomTask(coroutine_handle<promise_type>::from_promise(*this)); }
         suspend_never initial_suspend() { return { }; }
         suspend_never final_suspend() noexcept { return { }; }
         void return_void() { }
         void unhandled_exception() { }
     };
+
+    coroutine_handle<> handle;
+    CustomTask(coroutine_handle<> handle) : handle(handle) {
+        
+    }
 };
 
 CustomTask exampleCoroutine() {
@@ -23,7 +30,10 @@ CustomTask exampleCoroutine() {
     cout << "Statement B" << endl;
 }
 
+//NOTE: See tutorial series: https://www.youtube.com/watch?v=soHQAVYTmCE&list=PL2EnPlznFzmhKDBfE0lqMAWyr74LZsFVY&index=1
 int main() {
-    exampleCoroutine();
+    CustomTask task = exampleCoroutine();
+    task.handle.resume();
+    cout << "The coroutine is now done? " << boolalpha << task.handle.done() << endl;
     return 0;
 }

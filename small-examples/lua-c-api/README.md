0255c7be45b6c4b0abcee0d2954f206be7d644ed
Carlos DaLomba <carlos@2kpixelstudios.net>
2026-01-03 07:44:52 -0600

    Got my first Lua C example working in C++!

---
+++ small-examples/lua-c-api/README.md
@@ -0,0 +1,6 @@
+# Lua C API Example
+Note that because we use the example.lua script alongside the build exe, it is important to run the build at its directory so relative file paths work correctly during execution of the program. For example,
+
+```console
+cd ./out/Debug && ./LuaExample.exe && cd -
+```
---
+++ small-examples/lua-c-api/src/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+extern "C" {
+    #include "lua/lua.h"
+    #include "lua/lauxlib.h"
+    #include "lua/lualib.h"
+}
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int main() {
+    lua_State* lua = luaL_newstate();
+    luaL_openlibs(lua);
+
+    if (luaL_dofile(lua, "example.lua") != LUA_OK) {
+        const char* error = lua_tostring(lua, -1);
+        cerr << "An error occurred while trying to run example.lua! " << error << endl;
+        lua_pop(lua, 1);
+    }
+
+    lua_close(lua);
+
+    cout << "Lua C Example Program!" << endl;
+    return 0;
+}

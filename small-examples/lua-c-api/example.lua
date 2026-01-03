37d89317b83a2c7be02111c315a823b5c36dbc1d
Carlos DaLomba <carlos@2kpixelstudios.net>
2026-01-03 08:06:05 -0600

    Updated Lua example with getting and setting basic variables

--- small-examples/lua-c-api/example.lua
+++ small-examples/lua-c-api/example.lua
@@ -1,1 +1,9 @@
-print("Hi from Lua!")
+hp = 97
+maxHP = 100
+username = "Lunar"
+isDefeated = false
+
+print("Hi from Lua! hp = " .. hp .. " / " .. maxHP)
+print("(Lua) otherValue = " .. tostring(otherValue))
+hp = hp - 5
+
